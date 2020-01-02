0db0b6c64bb5041a52da1a0bce0525b7530380dc
Adeet Patel <adeet.patel@gmail.com>
2020-01-02 11:30:36 -0500

    Tune the ANN

--- artificial-nn/ann.py
+++ artificial-nn/ann.py
@@ -203,3 +203,33 @@ accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train,
 # small differences between various accuracies).
 mean = accuracies.mean()
 variance = accuracies.std()
+
+# Tuning the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(output_dim = 6, init = "uniform", activation = "relu", input_dim = 11))
+    classifier.add(Dense(output_dim = 6, init = "uniform", activation = "relu"))
+    classifier.add(Dense(output_dim = 1, init = "uniform", activation = "sigmoid"))
+    classifier.compile(optimizer = optimizer, loss = "binary_crossentropy", metrics = ["accuracy"])
+    return classifier
+
+# Unlike the previous instance of KerasClassifier(), we don't specify a batch
+# size and number of epochs. That's because these are the parameters we want to
+# tune, so instead we will put those parameters into a GridSearchCV object that
+# we will create.    
+classifier = KerasClassifier(build_fn = build_classifier)
+
+# This dictionary contains all combinations of the hyperparameters we want to
+# try, and see which set of hyperparameters is the optimal one.
+parameters = {"batch_size": [25, 32],
+              "nb_epoch": [100, 500],
+              "optimizer": ["adam", "rmsprop"]} # rmsprop is a very good optimizer for regular NNs
+grid_search = GridSearchCV(estimator = classifier, param_grid = parameters,
+                           scoring = "accuracy", cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
