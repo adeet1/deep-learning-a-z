623064c565b3662c70e3cebcda0ecdcc3d7ff0c1
Adeet Patel <adeet.patel@gmail.com>
2020-01-09 23:06:29 -0500

    Minor fix in updating self.W

--- boltzmann-machine/boltzmann.py
+++ boltzmann-machine/boltzmann.py
@@ -147,7 +147,8 @@ class RBM():
         return p_v_given_h, torch.bernoulli(p_v_given_h)
     
     def train(self, v0, vk, ph0, phk):
-        self.W += torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)
+        self.W += (torch.mm(v0.t(),ph0) - torch.mm(vk.t(),phk)).t()
+        # Old code: """self.W += torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)"""
         self.b += torch.sum((v0 - vk), 0) # keeps the format of b as a tensor of 2 dimensions
         self.a += torch.sum((ph0 - phk), 0)
     
